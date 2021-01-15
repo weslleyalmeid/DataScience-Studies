981646b7cdbdcaa5f4a2229964d6d44ea92fe158
Weslley Almeida <weslley_fac@hotmail.com>
2021-01-14 21:16:06 -0300

    alter prefict mode

--- Vendas - Meigarom/src/models/predict_model.py
+++ Vendas - Meigarom/src/models/predict_model.py
@@ -4,11 +4,12 @@ import os.path
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import cross_val_score, cross_val_predict
 
-
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 from catboost import CatBoostClassifier
+
+np.random.seed(42)
 
 DIR_FILE = os.path.abspath('.')
 DIR_DATA = os.path.join(DIR_FILE, 'data')
@@ -17,7 +18,7 @@ DIR_BASE = os.path.join(DIR_DATA, 'processed')
 df_train = pd.read_csv(os.path.join(DIR_BASE, 'train.csv'))
 df_test = pd.read_csv(os.path.join(DIR_BASE, 'test.csv'))
 
-X_train = df_train.drop(labels=['id', 'Response'], axis=1)
+X_train = df_train.drop(labels=['Response'], axis=1)
 X_test = df_test.drop(labels=['id', 'Response'], axis=1)
 y_train = df_train['Response']
 y_test = df_test['Response']
@@ -26,15 +27,30 @@ estimators = [
     RandomForestClassifier(),
     XGBClassifier(),
     LGBMClassifier(),
-    CatBoostClassifier()
+    CatBoostClassifier(verbose=False)
 ]
 
 for estimator in estimators:
     estimator.fit(X_train, y_train)
-
+    result = estimator.predict(X_test)
     name = estimator.__class__.__name__
 
-    result = estimator.predict(X_test)
+    # accuracy = cross_val_score(estimator, X_train, y_train, cv=5, scoring='accuracy', n_jobs=3).mean()
+    # precicion = cross_val_score(estimator, X_train, y_train, cv=5, scoring='precicion', n_jobs=3).mean()
+    # f1 = cross_val_score(estimator, X_train, y_train, cv=5, scoring='f1', n_jobs=3).mean()
+    # recall = cross_val_score(estimator, X_train, y_train, cv=5, scoring='recall', n_jobs=3).mean()
+    # roc_auc = cross_val_score(estimator, X_train, y_train, cv=5, scoring='roc_auc', n_jobs=3).mean()
+
+    print('Estimator: ' + name)
+    print(classification_report(y_test, result))
+    print(confusion_matrix(y_test, result))
+
+    # print('accuracy: ' + accuracy)
+    # print('precicion: ' + precicion)
+    # print('f1: ' + f1)
+    # print('recall: ' + recall)
+    # print('roc_auc: ' + roc_auc)
+    
 
 
 
