8e98f309ad7933ea6a947873085b70aa5a1ae2a2
Weslley Almeida <weslley_fac@hotmail.com>
2021-01-18 21:44:55 -0300

    Random Forest Fine Tunning

--- Vendas - Meigarom/src/models/predict_model.py
+++ Vendas - Meigarom/src/models/predict_model.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import os.path
 from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.model_selection import cross_val_score, cross_val_predict, RandomizedSearchCV
 
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
@@ -52,4 +52,40 @@ for estimator in estimators:
     # print('roc_auc: ' + roc_auc)
 
 
-# Verificando os algoritmos sem fine-tuning o algoritmo de random forest se saiu melhor
+# Random forest e CatBoost se sairam melhores quanto a classificação nos dados de testes, por esse motivo será utilizado o fine-tuning em ambos para verificar
+# qual irá perfomar melhor.
+
+paramers = {
+    'rfc': {
+        'n_estimators': [10, 50, 100, 200],
+        'max_features': ['auto', 'sqrt'],
+        'max_depth': [8, 16, 32],
+        'min_samples_split': [5, 10],
+        'min_samples_leaf': [1, 2, 4],
+        'bootstrap': [True, False]
+    },
+
+    'cb': {
+        'depth': [2, 4, 8, 10],
+        'iterations': [250, 100, 500, 1000],
+        'learning_rate': [0.03, 0.001, 0.01, 0.1, 0.2, 0.3],
+        'l2_leaf_reg': [3, 1, 5, 10, 100],
+        'border_count': [32, 5, 10, 20, 50, 100, 200],
+        'ctr_border_count': [50, 5, 10, 20, 100, 200],
+        'thread_count': 4
+    }
+}
+
+rfc = RandomForestClassifier()
+rf_random = RandomizedSearchCV(estimator=rfc, param_distributions=paramers['rfc'], n_iter=100, cv=3, verbose=1, random_state=42, n_jobs=-1)
+rf_random.fit(X_train, y_train)
+
+# RandomForestClassifier(bootstrap=False, max_depth=32, max_features='sqrt',
+#                        min_samples_split=5, n_estimators=200)
+
+# rfc_best = {'n_estimators': 200,
+#  'min_samples_split': 5,
+#  'min_samples_leaf': 1,
+#  'max_features': 'sqrt',
+#  'max_depth': 32,
+#  'bootstrap': False}
