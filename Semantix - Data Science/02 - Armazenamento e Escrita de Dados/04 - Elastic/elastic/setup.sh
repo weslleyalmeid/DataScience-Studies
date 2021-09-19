590db9bb09a94acdcac2d17cfa10af60177c4d27
weslleyalmeid <weslley_fac@hotmail.com>
2021-09-19 20:27:26 -0300

    alter vm.max

--- Semantix - Data Science/02 - Armazenamento e Escrita de Dados/04 - Elastic/elastic/setup.sh
+++ Semantix - Data Science/02 - Armazenamento e Escrita de Dados/04 - Elastic/elastic/setup.sh
@@ -3,4 +3,4 @@ docker pull docker.elastic.co/kibana/kibana:7.9.2
 docker pull docker.elastic.co/logstash/logstash:7.9.2
 
 # tee - redireciona o comando echo em modo sudo para arquivo
-echo "vm.max_map_count=12" | sudo tee -a /etc/sysctl.conf
+echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf
