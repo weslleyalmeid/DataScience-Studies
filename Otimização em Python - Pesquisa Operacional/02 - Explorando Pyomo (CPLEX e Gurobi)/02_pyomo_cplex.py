f0deb1213eac24332620555b1d93ae1346e99e6a
weslleyalmeid <weslley_fac@hotmail.com>
2022-02-23 21:18:54 -0300

    PO: CPLEX e Gurobi

---
+++ Otimização em Python - Pesquisa Operacional/02 - Explorando Pyomo (CPLEX e Gurobi)/02_pyomo_cplex.py
@@ -0,0 +1,32 @@
+import pyomo.environ as pyo
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+
+model = pyo.ConcreteModel()
+
+model.x = pyo.Var(bounds=(0,10))
+model.y = pyo.Var(bounds=(0,10))
+x = model.x
+y = model.y
+
+model.C1 = pyo.Constraint(expr= -x+2*y<=8)
+model.C2 = pyo.Constraint(expr= 2*x+y<=14)
+model.C3 = pyo.Constraint(expr= 2*x-y<=10)
+
+model.obj = pyo.Objective(expr= x+y, sense=maximize)
+
+#opt = SolverFactory('glpk')
+#opt = SolverFactory('gurobi')
+
+opt = SolverFactory('cplex', executable='/opt/ibm/ILOG/CPLEX_Studio201/cplex/bin/x86-64_linux/cplex')
+
+opt.solve(model)
+
+model.pprint()
+
+x_value = pyo.value(x)
+y_value = pyo.value(y)
+
+print('\n---------------------------------------------------------------------')
+print('x=',x_value)
+print('y=',y_value)
