7d0a9080e420525dbb3e2966b52f428a7ebf998c
weslleyalmeid <weslley_fac@hotmail.com>
2022-02-24 22:10:03 -0300

    PO: problemas complexos com pyomo e solvers gurobi e scip

---
+++ Otimização em Python - Pesquisa Operacional/02 - Explorando Pyomo (CPLEX e Gurobi)/05_ortools_others_solvers.py
@@ -0,0 +1,31 @@
+import ortools.linear_solver.pywraplp as otlp
+
+solver = otlp.Solver('teste',otlp.Solver.SCIP_MIXED_INTEGER_PROGRAMMING)
+
+
+# variables
+# lower limit and upper limit
+x = solver.NumVar(0,10,'x')
+y = solver.NumVar(0,10,'y')
+
+
+# restrictions
+solver.Add(-x + 2*y <= 8)
+solver.Add(2*x + y <= 14)
+solver.Add(2*x - y <= 10)
+
+
+# objective function
+solver.Maximize(x + y)
+
+results = solver.Solve()
+
+# check if the best solution
+if results == otlp.Solver.OPTIMAL:
+    print('Resultado Encontrado')
+else:
+    print('Resultado NÃO Encontrado')
+    
+# show result
+print('x=',x.solution_value())
+print('y=',y.solution_value())
